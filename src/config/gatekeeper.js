362043935457bed5dce208c7a031321a56c3d1d3
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-07-25 15:55:51 +0200

    Created gatekeeper config

---
+++ src/config/gatekeeper.js
@@ -0,0 +1,12 @@
+{
+	"points": {
+		"start": +200,
+		"query": -1,
+		"validLink": -1,
+		"invalidLink": -40,
+		"validCreateLink": -40,
+		"invalidCreateLink": -10
+	},
+	"resetTime": 1,
+	"banTime": 10
+}
