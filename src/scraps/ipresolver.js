ec9875bda1890a9271428a8ac092ff6dc70c704f
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-07-31 10:52:00 +0200

    Prepared basic skeletron

--- src/scraps/ipresolver.js
+++ src/scraps/ipresolver.js
@@ -5,3 +5,42 @@
 * by Stefano Vazzoler (stefanovazzocell@gmail.com)
 * https://stefanovazzoler.com/
 */
+
+// Load config
+var { use_connection_ip, use_proxy, use_cloudflare } = require('./config/ipresolver');
+
+/*
+* getCloudflare(req) - gets the ip from cloudflare if allowed or the next available option
+*
+* @requires req to be a valid express request
+* @returns ip string
+*/
+function getCloudflare(req) {
+	// body...
+}
+
+/*
+* getProxy(req) - gets the ip from the proxy if allowed or the next available option
+*
+* @requires req to be a valid express request
+* @returns ip string
+*/
+function getProxy(req) {
+	// body...
+}
+
+/*
+* getRequest(req) - gets the ip from the request if allowed or returns "unknown"
+*
+* @requires req to be a valid express request
+* @returns ip string
+*/
+function getRequest(req) {
+	// body...
+}
+
+// Make public function accessible
+module.exports = {
+	// When the users requires an ip, start looking for cloudflare
+	getIp: getCloudflare
+}
