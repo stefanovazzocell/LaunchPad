415774ba4298f039c37bbb3958bc3ecab1a33633
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-08-02 11:22:11 +0200

    Added and improved check herlpers

--- src/scraps/api.js
+++ src/scraps/api.js
@@ -29,7 +29,7 @@ function requestError(res, msg='Something is wrong with your request, try refres
 }
 
 /*
-* isAvailable(toCheck) - Check if all variables in array are set
+* isAvailable(toCheck, res) - Check if all variables in array are set
 *
 * @requests toCheck is array of variables to check
 * @requests res is a express response
@@ -46,6 +46,28 @@ function isAvailable(toCheck, res) {
 }
 
 /*
+* assertTrue(toCheck, checks, res) - Check if isAvailable toCheck produces true and all the functions in array produce true
+*
+* @requests toCheck is array of variables to check
+* @requests checks is array of functions to check
+* @requests res is a express response
+* @return bool true if all true, false otherwise
+*/
+function assertTrue(toCheck, checks, res) {
+	if (isAvailable(toCheck, res)) {
+		for (var i = 0; i < toCheck.length; i++) {
+			if (!toCheck[i]()) {
+				requestError(res);
+				return false;
+			}
+		}
+		return true;
+	} else {
+		return false
+	}
+}
+
+/*
 * Calls
 */
 
@@ -56,7 +78,7 @@ function isAvailable(toCheck, res) {
 * @requires res from expressjs' request
 */
 function api_get(req, res) {
-	if (isAvailable([req.body.link, req.body.data, req.body.parameters], res)) {
+	if (assertTrue([req.body.link], [], res)) {
 		res.send("ok");
 	}
 }
@@ -68,7 +90,9 @@ function api_get(req, res) {
 * @requires res from expressjs' request
 */
 function api_set(req, res) {
-	//
+	if (assertTrue([req.body.link, req.body.data, req.body.parameters], [], res)) {
+		res.send("ok");
+	}
 }
 
 // Make public function accessible
